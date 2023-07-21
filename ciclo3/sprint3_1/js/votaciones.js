1ef80103efb9114a606c24706823fb6aec83a60d
Laura Espitia <113865102+laura-DGM@users.noreply.github.com>
2023-07-20 19:47:29 -0500

    Add files via upload

---
+++ ciclo3/sprint3_1/js/votaciones.js
@@ -0,0 +1,19 @@
+
+function VIA() {
+    alert("votaste por Isabel Agudelo");
+    window.history.back();
+}
+function VAM() {
+    alert("votaste por Andrea Monotoya");
+    window.history.back();
+
+}
+function VAB() {
+    alert("votaste por Antonia Bustamante");
+    window.history.back();
+}
+function VJI() {
+    alert("votaste por Juan Isaza");
+    window.history.back();
+}
+
